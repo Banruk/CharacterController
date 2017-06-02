e145c3ae7172530957e4a35ed80809e6cdd06961
Banruk <lucasmorrish22@gmail.com>
2017-06-01 21:35:57 -0400

    Added jumping and changed some core functions

--- CharacterController.cs
+++ CharacterController.cs
@@ -3,19 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour {
-    //Settings variable declaration
-    public float inputDelay = 0.1f;
-    public float forwardVelocity = 12;
-    public float rotationalVelocity = 100;
+    
+    [System.Serializable]
+    public class MoveSettings
+    {
+        public float forwardVelocity = 12;
+        public float rotationalVelocity = 100;
+        public float jumpVelocity = 25;
+        public float distanceToGrounded = 0.51f;
+        public LayerMask ground;
+    }
 
+    [System.Serializable]
+    public class PhysSettings
+    {
+        public float downwardAcceleration = 0.75f;
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public float inputDelay = 0.1f;
+        public string FORWARD_AXIS = "Vertical";
+        public string TURN_AXIS = "Horizontal";
+        public string JUMP_AXIS = "Jump";
+    }
+
+    //ensuring the classes can be seen and modified by the inspector
+    public MoveSettings moveSetting = new MoveSettings();
+    public PhysSettings physSetting = new PhysSettings();
+    public InputSettings inputSetting = new InputSettings();
+
+    Vector3 velocity = Vector3.zero;
     Quaternion targetRotation;
     Rigidbody rigidBody;
-    float forwardInput, turnInput;
+    float forwardInput, turnInput, jumpInput;
 
 
     public Quaternion TargetRotation
     {
         get { return targetRotation; }
+    }
+
+    bool Grounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, moveSetting.distanceToGrounded, moveSetting.ground);
     }
  
 	void Start ()
@@ -26,13 +58,14 @@ public class CharacterController : MonoBehaviour {
         else
             Debug.LogError("The character needs a rigidbody.");
 
-        forwardInput = turnInput = 0;
+        forwardInput = turnInput = jumpInput = 0;
     }
 
     void GetInput()
     {
-        forwardInput = Input.GetAxis("Vertical");
-        turnInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis(inputSetting.FORWARD_AXIS);//interpolated
+        turnInput = Input.GetAxis(inputSetting.TURN_AXIS);//interpolatd
+        jumpInput = Input.GetAxisRaw(inputSetting.JUMP_AXIS);//non-interpolated
     }
 
 	void Update ()
@@ -44,26 +77,47 @@ public class CharacterController : MonoBehaviour {
     void FixedUpdate()
     {
         Run();
+        Jump();
+
+        rigidBody.velocity = transform.TransformDirection(velocity);
     }
 
     void Run()
     {
-        if (Mathf.Abs(forwardInput) > inputDelay)
+        if (Mathf.Abs(forwardInput) > inputSetting.inputDelay)
         {
             //move
-            rigidBody.velocity = transform.forward * forwardInput * forwardVelocity;
+            velocity.z = moveSetting.forwardVelocity * forwardInput;
         }
         else
             //zero velocity
-            rigidBody.velocity = Vector3.zero;
+            velocity.z = 0;
     }
 
     void Turn()
     {
-        if (Mathf.Abs(turnInput) > inputDelay)
+        if (Mathf.Abs(turnInput) > inputSetting.inputDelay)
         {
-            targetRotation *= Quaternion.AngleAxis(rotationalVelocity * turnInput * Time.deltaTime, Vector3.up);
+            targetRotation *= Quaternion.AngleAxis(moveSetting.rotationalVelocity * turnInput * Time.deltaTime, Vector3.up);
         }
         transform.rotation = targetRotation;
+    }
+
+    void Jump()
+    {
+        if (jumpInput > 0 && Grounded())
+        {
+            //jump
+            velocity.y = moveSetting.jumpVelocity;
+        }
+        else if (jumpInput == 0 && Grounded()){
+            //zero out velocity
+            velocity.y = 0;
+        }
+        else
+        {
+            //decrease velocity.y
+            velocity.y -= physSetting.downwardAcceleration;
+        }
     }
 }
