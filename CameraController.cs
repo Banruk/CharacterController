2c8de10af99dfa181c05a69cd1699dbc594b1ec4
Banruk <lucasmorrish22@gmail.com>
2017-06-19 23:45:03 -0400

    Added camera rotation and zoom controls

--- CameraController.cs
+++ CameraController.cs
@@ -5,17 +5,55 @@ using UnityEngine;
 public class CameraController : MonoBehaviour {
 
     public Transform target;
-    public float lookSmooth = 0.09f;
-    public Vector3 offsetFromTarget = new Vector3(0, 1, -8);
-    public float xTilt = 10;
+    [System.Serializable]
+    public class PositionSettings
+    {
+        public Vector3 targetPosOffset = new Vector3(0, 3.4f, 0);   //declares artificial origin of the character to target with the camera
+        public float lookSmooth = 100f;                             //Smoothing for camera movement
+        public float distanceFromTarget = -8;                       //Distance from the target
+        public float zoomSmooth = 10;                               //Zoom smoothing
+        public float maxZoom = -2;                                  //Maximum zoom distance
+        public float minZoom = -15;                                 //Minimum zoom distance
+    }
 
+    [System.Serializable]
+    public class OrbitSettings
+    {
+        public float xRotation = -20;                               //x rotation modifier
+        public float yRotation = -180;                              //y rotation modifier
+        public float maxXRotation = 25;                             //maximum x rotation to stop bad camera angles
+        public float minXRotation = -85;                            //maximum y rotation to stop bad camera angles
+        public float vOrbitSmooth = 150;                            //Vertical orbit smoothing
+        public float hOrbitSmooth = 150;                            //Horizontal orbit smoothing
+    }
+
+    [System.Serializable]
+    public class InputSettings
+    {
+        public string ORBIT_HORIZONTAL_SNAP = "OrbitHorizontalSnap";//snaps y rotation to behind the target
+        public string ORBIT_HORIZONTAL = "OrbitHorizontal";         //rotates the camera on the y
+        public string ORBIT_VERTICAL = "OrbitVertical";             //rotates the camera on the X
+        public string ZOOM = "Mouse ScrollWheel";                   //sets zoom distance, NOTE most likely removing player control over this. 
+    }
+
+    public PositionSettings position = new PositionSettings();
+    public OrbitSettings orbit = new OrbitSettings();
+    public InputSettings input = new InputSettings();
+
+    Vector3 targetPos = Vector3.zero;
     Vector3 destination = Vector3.zero;
     CharacterController charController;
-    float rotationalVelocity = 0;
+    float vOrbitInput, hOrbitInput, zoomInput, hOrbitSnapInput;
 
     private void Start()
     {
-        SetCameraTarget(target);   
+        SetCameraTarget(target);
+
+        //Placed here to ensure the camera starts in the right position
+        targetPos = target.position + position.targetPosOffset;
+        destination = Quaternion.Euler(orbit.xRotation, orbit.yRotation, 0) * -Vector3.forward * position.distanceFromTarget;
+        destination += targetPos;
+        transform.position = destination;
     }
 
     void SetCameraTarget(Transform t)
@@ -35,6 +73,21 @@ public class CameraController : MonoBehaviour {
             Debug.LogError("Your camera needs a target.");
     }
 
+    void GetInput()
+    {
+        vOrbitInput = Input.GetAxisRaw(input.ORBIT_VERTICAL);
+        hOrbitInput = Input.GetAxisRaw(input.ORBIT_HORIZONTAL);
+        hOrbitSnapInput = Input.GetAxisRaw(input.ORBIT_HORIZONTAL_SNAP);
+        zoomInput = Input.GetAxisRaw(input.ZOOM);
+    }
+
+    void Update()
+    {
+        GetInput();
+        OrbitTarget();
+        ZoomInOnTarget();
+    }
+
     void LateUpdate()
     {
         //moving
@@ -45,14 +98,49 @@ public class CameraController : MonoBehaviour {
 
     void MoveToTarget()
     {
-        destination = charController.TargetRotation * offsetFromTarget;
-        destination += target.position;
+        targetPos = target.position + position.targetPosOffset;
+        destination = Quaternion.Euler(orbit.xRotation, orbit.yRotation + target.eulerAngles.y, 0) * -Vector3.forward * position.distanceFromTarget;
+        destination += targetPos;
         transform.position = destination;
     }
 
     void LookAtTarget()
     {
-        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotationalVelocity, lookSmooth);
-        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+        Quaternion targetRotation = Quaternion.LookRotation(targetPos - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, position.lookSmooth * Time.deltaTime);
+    }
+
+    void OrbitTarget()
+    {
+        if(hOrbitSnapInput > 0)
+        {
+            orbit.yRotation = -180;
+        }
+
+        orbit.xRotation += -vOrbitInput * orbit.vOrbitSmooth * Time.deltaTime;
+        orbit.yRotation += -hOrbitInput * orbit.hOrbitSmooth * Time.deltaTime;
+
+        if(orbit.xRotation > orbit.maxXRotation)
+        {
+            orbit.xRotation = orbit.maxXRotation;
+        }
+
+        if(orbit.xRotation < orbit.minXRotation)
+        {
+            orbit.xRotation = orbit.minXRotation;
+        }
+    }
+
+    void ZoomInOnTarget()
+    {
+        position.distanceFromTarget += zoomInput * position.zoomSmooth * Time.deltaTime;
+        if (position.distanceFromTarget > position.maxZoom)
+        {
+            position.distanceFromTarget = position.maxZoom;
+        }
+        if (position.distanceFromTarget < position.minZoom)
+        {
+            position.distanceFromTarget = position.minZoom;
+        }
     }
 }
